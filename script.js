7808fc6419cb8cff2611e9dcf6a8260f742130d7
Baymanov Namat <namataxvd@gmail.com>
2021-12-29 14:23:55 +0600

    add html

---
+++ script.js
@@ -0,0 +1,35 @@
+var studentFirstName = "Bakyt";
+var studentLastName = "Utanov";
+var studentAge = 20;
+var studentOccupied = false;
+
+var student = {
+  firstName: "Bakyt",
+  lastName: "Utanov",
+  age: 20,
+  occuptied: false,
+
+};
+
+
+// output
+// console.log(student.firstName + " " + student.lastName);
+
+
+var dog = {
+  name: "Kumayik",
+  age: 2,
+};
+
+
+// update
+dog.age = 3;
+// console.log(dog.age);
+
+// delete 
+delete dog.color;
+
+
+
+
+
